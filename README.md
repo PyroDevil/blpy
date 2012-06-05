3501c3dfc36213f5f34501775e0b967c27eed421
PyroDevil <p.devil@gmail.com>
2012-06-05 16:27:55 +0200

    First try with persistent storage

--- README.md
+++ README.md
@@ -4,6 +4,21 @@ blpy
 A small python script for downloading different blocklists, merging them to
 "meta" blocklists and hosting them on the local machine.
 
+It uses Python 2.7 with Twisted, so you need that.
+
+I programmed it to be lazy, so it should fetch all blacklists on the first
+request and saves them into the ram for the following requests.
+
+It should be fast, after all blacklists are within the ram, but it is not very
+optimized on memory usage. (Blacklists are stored within the ram twice, gziped
+and as plain text.)
+
+What is this for?
+-----------------
+
+Personally I use this for the bittorrent-client "Transmission", but maybe it can
+be used for other things too.
+
 Configuration
 -------------
 
@@ -14,5 +29,13 @@ Running blpy
 
 Run blpy by executing `main.py` with your python interpreter. The first argument
 should be the path to your configuration file.
+
+Known Bugs
+----------
+
+I tried to save the blacklists persistent on the disk after exit, but this
+doesn't work very well at the moment. I think there are difficulties with open
+connections. Maybe "Twisted"s ressource objects are not very good pickable.
+So the next step is to try split the twisted ressources and the blacklist data.
 
 
--- src/blpy/Application.py
+++ src/blpy/Application.py
@@ -4,7 +4,11 @@ Created on 09.04.2012
 @author: pyro
 '''
 
+import os
+import time
+import signal
 from ConfigParser import SafeConfigParser
+from pickle import Pickler, Unpickler
 
 from twisted.internet import reactor
 from twisted.web import server
@@ -12,7 +16,19 @@ from twisted.web import server
 from Client import incomming_blacklist
 from Server import outgoing_blacklist, root_resource
 
-def StartServer(configfilepath):
+incomming = dict()
+outgoing = dict()
+config = None
+
+def shutdown_handler(signum, frame):
+    if reactor.running:
+        print("Send Reactor stop")
+        reactor.callFromThread(reactor.stop)
+
+
+def StartServer(configfilepath, load_persistent=False, save_persistent=False):
+    global incomming, outgoing, config
+
     config = SafeConfigParser(allow_no_value=True)
     config.read(configfilepath)
 
@@ -21,27 +37,40 @@ def StartServer(configfilepath):
 
     listen_port = config.getint('general', 'listen_port')
 
-    incomming = dict()
-    outgoing = dict()
+    persistent_storage = config.get('general', 'persistent_storage')
 
-    for sec in config.sections():
-        if sec.startswith('inc_'):
-            name = config.get(sec, 'name')
-            url = config.get(sec, 'url')
-            days = config.getfloat(sec, 'update_days')
-            incomming[name] = incomming_blacklist(name, url, days)
+    #signal.signal(signal.SIGTERM, shutdown_handler)
+    #signal.signal(signal.SIGQUIT, shutdown_handler)
 
-    for sec in config.sections():
-        if sec.startswith('out_'):
-            name = config.get(sec, 'name')
-            lists = list()
-            for i in config.items(sec):
-                if i[0].startswith('list_'):
-                    lists.append(incomming[i[1]])
-            outgoing[name] = outgoing_blacklist(name, lists)
+    if (load_persistent and
+        os.path.exists(persistent_storage) and
+        os.path.getmtime(configfilepath) <=
+        os.path.getmtime(persistent_storage)):
+        with open(persistent_storage, 'rb') as persistent_file:
+            (incomming, outgoing) = Unpickler(persistent_file).load()
+    else:
+        for sec in config.sections():
+            if sec.startswith('inc_'):
+                name = config.get(sec, 'name')
+                url = config.get(sec, 'url')
+                days = config.getfloat(sec, 'update_days')
+                incomming[name] = incomming_blacklist(name, url, days)
+
+        for sec in config.sections():
+            if sec.startswith('out_'):
+                name = config.get(sec, 'name')
+                lists = list()
+                for i in config.items(sec):
+                    if i[0].startswith('list_'):
+                        lists.append(incomming[i[1]])
+                outgoing[name] = outgoing_blacklist(name, lists)
 
     root = root_resource(outgoing.values(), incomming.values())
 
     reactor.listenTCP(listen_port, server.Site(root), interface=listen_addr)
     reactor.run()
+
+    if save_persistent :
+        with open(persistent_storage, 'wb') as persistent_file:
+            Pickler(persistent_file).dump((incomming, outgoing))
 
